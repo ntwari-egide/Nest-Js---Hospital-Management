9a0b0f830576956ac3631e7d78d7d1e15f662e1f
yombi <ntwariegide2@gmail.com>
2021-05-03 21:20:19 +0200

    done with assigning data to the url

--- src/doctors.controler.ts
+++ src/doctors.controler.ts
@@ -2,21 +2,25 @@ import { Controller, Get } from '@nestjs/common'
 
 @Controller('/doctors')
 export default class DoctorsController{
-    listOfDoctors : [
-        {
-            name: "ntwari egide",
-            role: "doctor",
-            clinic: "gahunga santre de cante"
-        },
-        {
-            name: "mugisha jules",
-            role: "phd-doctor",
-            clinic: "gahunga santre de cante"
-        }
-    ]
-    
+    listOfDoctors : object[]
+
+    constructor(){
+        this.listOfDoctors =[
+            {
+                name: "ntwari egide",
+                role: "doctor",
+                clinic: "gahunga santre de cante"
+            },
+            {
+                name: "mugisha jules",
+                role: "phd-doctor",
+                clinic: "gahunga santre de cante"
+            }
+        ]
+    }
+
     @Get()
     findAll(){
-        return 
+        return this.listOfDoctors
     }
 }
