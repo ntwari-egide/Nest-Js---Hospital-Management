f20079a7011746e9c1dfe992b7e55feb937e412f
yombi <ntwariegide2@gmail.com>
2021-05-03 21:38:36 +0200

    working with services in nest js

--- src/doctors.service.ts
+++ src/doctors.service.ts
@@ -1,1 +1,18 @@
+import { Injectable } from "@nestjs/common";
+
 // services manage data retrieval and storage logics
+@Injectable
+export class DoctorsService{
+    listOfDoctors : [
+        {
+            name: "ntwari egide",
+            role: "doctor",
+            clinic: "gahunga santre de cante"
+        },
+        {
+            name: "mugisha jules",
+            role: "phd-doctor",
+            clinic: "gahunga santre de cante"
+        }
+    ]
+}
