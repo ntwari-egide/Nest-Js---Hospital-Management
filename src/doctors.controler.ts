c19a6d2adf7c3e59fd03dc2bfc25a9c377e7c443
yombi <ntwariegide2@gmail.com>
2021-05-03 21:13:39 +0200

    working with request handling

--- src/doctors.controler.ts
+++ src/doctors.controler.ts
@@ -1,1 +1,13 @@
-import { Controller } from '@nestjs/core'
+import { Controller, Get } from '@nestjs/common'
+
+@Controller('/doctors')
+export default class DoctorsController{
+    @Get()
+    findAll(){
+        return [{
+            name: "ntwari egide",
+            role: "doctor",
+            clinic: "gahunga santre de cante"
+        }]
+    }
+}
