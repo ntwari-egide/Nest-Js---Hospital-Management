3e280f6a83b81cb095e846d6d61efc79df9825e0
yombi <ntwariegide2@gmail.com>
2021-05-04 15:02:21 +0200

    solving error in the contructors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { DoctorsService } from './doctors.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,6 +7,6 @@ import DoctorsController from './doctors.controller';
 @Module({
   imports: [],
   controllers: [AppController,DoctorsController],
-  providers: [AppService],
+  providers: [AppService,DoctorsService],
 })
 export class AppModule {}
