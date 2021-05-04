7b9d82d1b03eb9ddeb490c947ca69c6460b27dd3
yombi <ntwariegide2@gmail.com>
2021-05-04 15:53:56 +0200

    global middleware is working

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { globalLogger } from './doctors.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   /* adding global middleware */
+  app.use(globalLogger)
   await app.listen(3000);
 }
 bootstrap();
