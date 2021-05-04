70eb4cb7d6df05b58362b2aa3da04afbd57eac51
yombi <ntwariegide2@gmail.com>
2021-05-04 15:53:30 +0200

    adding global middleware

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /* adding global middleware */
   await app.listen(3000);
 }
 bootstrap();
