09d176405e8a475e63435c67aab3781a554f1f7a
yombi <ntwariegide2@gmail.com>
2021-05-04 15:20:43 +0200

    tested and working well

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { AppService } from './app.service';
 import DoctorsController from './doctors.controller';
 
 @Module({
-  imports: [],
+  imports: [DoctorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
