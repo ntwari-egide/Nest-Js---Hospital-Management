135e729571f78014ccf6300874dfcb197fc8d54f
yombi <ntwariegide2@gmail.com>
2021-05-04 16:04:08 +0200

    tested and working

--- src/doctors.controller.ts
+++ src/doctors.controller.ts
@@ -20,7 +20,6 @@ export default class DoctorsController{
         throw new HttpException('Unauthorized access',HttpStatus.FORBIDDEN)
     }
 
-    
     @Post()
     async addDoctor(@Body() doctor: Doctor){
         this.doctorService.createDoctor({name: 'new doctor',role: 'doctor',clinic: 'any clinic'})
