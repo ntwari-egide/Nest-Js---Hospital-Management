9f2885a988fa96541e6d78f3b88f03d8651b9e49
yombi <ntwariegide2@gmail.com>
2021-05-04 17:39:45 +0200

    adding global exception filters

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { HttpExceptionFilter } from './http-request.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { globalLogger } from './doctors.middleware';
@@ -6,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   /* adding global middleware */
   app.use(globalLogger)
+  /* adding global exception filters */
+  app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(3000);
 }
 bootstrap();
