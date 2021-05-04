285415a75e6fdeb1d9abea50d9d5bd6871b4c054
yombi <ntwariegide2@gmail.com>
2021-05-04 15:27:20 +0200

    done implementation of middleware

--- src/doctors.middleware.ts
+++ src/doctors.middleware.ts
@@ -1,6 +1,10 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction } from "express";
 
 @Injectable()
 export class LoggerDoctorMiddleware implements NestMiddleware{
-
+    use(req: Request,res: Response,next: NextFunction){
+        console.log("you are accessing doctors .......");
+        next()        
+    }
 }
