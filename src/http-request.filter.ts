6720547f6973daa3b73f4baa786550b05e96dd42
yombi <ntwariegide2@gmail.com>
2021-05-04 17:46:52 +0200

    handling all unhanded exceptions

--- src/http-request.filter.ts
+++ src/http-request.filter.ts
@@ -1,5 +1,7 @@
+import { Catch, HttpException, HttpStatus } from '@nestjs/common';
+import { ExceptionFilter } from '@nestjs/common';
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Request, Response, response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter  implements ExceptionFilter{
@@ -15,6 +17,25 @@ export class HttpExceptionFilter  implements ExceptionFilter{
                 statusCode: status,
                 timestamp: new Date().toISOString(),
                 path: request.url
+            })
+    }
+}
+
+/* caching all unhandled exceptions */
+@Catch()
+export class allExceptionsFilter extends ExceptionFilter{
+    Catch(exception: unknown,host : ArgumentsHost){
+        const ctx = host.switchToHttp()
+        const request = ctx.getRequest<Request>()
+        const response = ctx.getResponse<Response>()
+        const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR
+
+        response
+            .status(status)
+            .json({
+                statusCode: status,
+                timestamp: new Date().toISOString(),
+                path: request.url 
             })
     }
 }
