c6d2a4d6bfc73d7ebb993141294edae484b4d683
yombi <ntwariegide2@gmail.com>
2021-05-05 18:08:10 +0200

    exporting schema

--- src/doctor.schema.ts
+++ src/doctor.schema.ts
@@ -1,4 +1,4 @@
-import { Prop, Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
 export type DoctorDocument = Doctor & Document
@@ -14,6 +14,8 @@ export class Doctor{
     role: string
 
     @Prop()
-    
+
     clinic: string
 }
+
+export const DoctorSchema = SchemaFactory.createForClass(Doctor)
