1f51b3173225827215e47665cb23cf577a1ab669
yombi <ntwariegide2@gmail.com>
2021-06-11 20:39:57 +0200

    add class validator

--- src/dto/createdoctor.dto.ts
+++ src/dto/createdoctor.dto.ts
@@ -1,7 +1,12 @@
+import {  IsString } from "class-validator"
+
 export class CreateDoctorDto {
+    @IsString()
     name: string
 
+    @IsString()
     role: string
 
+    @IsString()
     clinic: string
 }
