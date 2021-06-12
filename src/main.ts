7a33d14b3610e27c679571286d2dd1d4f581c00c
yombi <ntwariegide2@gmail.com>
2021-06-12 20:30:36 +0200

    Added timeout interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { globalLogger } from './doctors.middleware';
 import { AuthGuard } from './guards/auth.guard';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { CacheInterceptor } from './interceptors/cache.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,8 @@ async function bootstrap() {
   app.useGlobalGuards(new AuthGuard())
 
   app.useGlobalInterceptors(new LoggingInterceptor())
+
+  app.useGlobalInterceptors(new CacheInterceptor())
 
   await app.listen(3000);
 }
